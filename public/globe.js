0669163e4c20b31d664f494f0202990bcbe308c3
Power2913 <shakti29.uts@gmail.com>
2024-02-10 13:37:49 +0530

    speed-G

--- public/globe.js
+++ public/globe.js
@@ -105,7 +105,7 @@ const setScene = () => {
 const setControls = () => {
   controls                 = new OrbitControls(camera, renderer.domElement);
   controls.autoRotate      = true;
-  controls.autoRotateSpeed = 10;
+  controls.autoRotateSpeed = 1000;
   controls.enableDamping   = true;
   controls.enableRotate    = true;
   controls.enablePan       = false;
