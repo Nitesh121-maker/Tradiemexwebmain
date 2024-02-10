a331791db02fc00c8106b476fc7f6fcdf156eb76
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-02-10 11:42:53 +0530

    Updated Code at  11:42 10-02-2024

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -2,7 +2,8 @@
 
 <p>Name: {{ $formData['name'] }}</p>
 <p>Email: {{ $formData['email'] }}</p>
-<p>Name: {{ $formData['number'] }}</p>
+<p>Country Region: {{ $formData['country_region'] }}</p>
+<p>Number: {{ $formData['number'] }}</p>
 <p>Company: {{ $formData['company'] }}</p>
 <p>Role: {{ $formData['role'] }}</p>
 <p>Message: {{ $formData['message'] }}</p>
