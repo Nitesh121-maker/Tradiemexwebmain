e6838b930de7bb66dc210083f0c8e01add87a40e
Power2913 <shakti29.uts@gmail.com>
2024-02-12 14:55:40 +0530

    SR_Commits-12-02-24

--- resources/views/frontend/link.blade.php
+++ resources/views/frontend/link.blade.php
@@ -6,5 +6,16 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/fontawesome.min.css" integrity="sha512-siarrzI1u3pCqFG2LEzi87McrBmq6Tp7juVsdmGY1Dr8Saw+ZBAzDzrGwX3vgxX1NkioYNCFOVC0GpDPss10zQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="canonical" href="https://www.tradeimex.in/"/>
+
 <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 <script src="https://www.google.com/recaptcha/api.js?render=6Ld1Ax8pAAAAAJWU_TaW2gMhMvAOeRl885SXqVPT"></script>
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NBN2JCV');
+</script>
+<!-- End Google Tag Manager -->
--- resources/views/frontend/script.blade.php
+++ resources/views/frontend/script.blade.php
@@ -2,6 +2,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
 
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NBN2JCV"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <!-- form js -->
     <script>
         grecaptcha.ready(() => {
@@ -51,5 +57,68 @@
             duration: 800
 
         });
+    </script>
+     <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "TradeImeX info solution Pvt Ltd",
+            
+                    "url": "https://www.tradeimex.in/",
+            "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "9319646667",
+                "contactType": "customer service",
+                "areaServed": "IN",
+                "availableLanguage": "en"
+            },
+            "sameAs": [
+                "https://www.facebook.com/tradeimex/",
+                "https://twitter.com/TradeImeX/",
+                "https://www.youtube.com/channel/UCTHU41uHt6xOub4XDy2Egxw",
+                "https://www.linkedin.com/company/tradeimex/",
+                "https://in.pinterest.com/tradeimex/",
+                "https://www.tradeimex.in/"
+            ]
+        }
+        {
+            "@context": "https://schema.org/",
+            "@type": "WebSite",
+            "name": "Tradeimex",
+            "url": "https://www.tradeimex.in/",
+            "potentialAction": {
+                "@type": "SearchAction",
+            "target": "https://www.tradeimex.in/{search_term_string}https://www.tradeimex.in/",
+                "query-input": "required name=search_term_string"
+            }
+        }
+        {
+            "@context": "https://schema.org",
+            "@type": "LocalBusiness",
+            "name": "TradeImex - Import Export Data Provider, Data Analytic & Shipment Services",
+            "image": "https://www.tradeimex.in/",
+            "@id": "",
+            "url": "https://www.tradeimex.in/",
+            "telephone": "9319646667",
+            "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "372, 3rd Floor, 110034, Block RU, Pitam Pura, New Delhi, Delhi 110034",
+                "addressLocality": "New Delhi",
+                "postalCode": "110034",
+                "addressCountry": "IN"
+            },
+            "openingHoursSpecification": {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday"
+                ],
+                "opens": "10:00",
+                "closes": "18:30"
+            }
+        }
     </script>
     <script src="//code.tidio.co/sdzqyzkqyjktbhjlcr0v8xbgipvxwtc9.js"></script>
