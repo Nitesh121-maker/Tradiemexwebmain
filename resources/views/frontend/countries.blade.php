78ef903ae84e7585f8c007cdc01c794999cd2dfa
Power2913 <shakti29.uts@gmail.com>
2024-02-16 17:21:24 +0530

    SR_Commits-16-02-24

--- resources/views/frontend/countries.blade.php
+++ resources/views/frontend/countries.blade.php
@@ -905,7 +905,7 @@
                 labels: [countryNames[0], countryNames[1], countryNames[2], countryNames[3], countryNames[4], countryNames[5], countryNames[6], countryNames[7], countryNames[8]],
                 datasets: [{
                     label: '{{$country->cp_heading}}',
-                    data: [percentageData[0], percentageData[1], percentageData[2], percentageData[3], percentageData[4],percentageData[5], percentageData[6], percentageData[7],percentageData[8], percentageData[9]],percentageData[10]]
+                    data: [percentageData[0], percentageData[1], percentageData[2], percentageData[3], percentageData[4],percentageData[5], percentageData[6], percentageData[7],percentageData[8], percentageData[9] ,percentageData[10]]
                     borderWidth: 1
                 }]
                 },
