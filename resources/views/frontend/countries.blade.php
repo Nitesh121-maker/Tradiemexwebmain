5f14065f51ec7230f2c80525f65efc57be099ab0
Power2913 <shakti29.uts@gmail.com>
2024-02-16 18:04:07 +0530

    SR_Commits-16-02-24

--- resources/views/frontend/countries.blade.php
+++ resources/views/frontend/countries.blade.php
@@ -430,12 +430,12 @@
             </div>
             <div class="container pdt-2">
                 <div  class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
+                    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                         <div style="justify-content: center;display: flex;">
                             <canvas id="10_partners"></canvas>
                         </div>
                     </div>
-                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-4">
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
                         @php
                         $partners = explode("\n", $country->country_partner_name);
                         @endphp
