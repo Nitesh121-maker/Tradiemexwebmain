d117e0944a8bacbb8318819cab7e693d5c9b5ec4
Nitesh Chauhan <72297535+Nitesh121-maker@users.noreply.github.com>
2024-02-16 17:26:16 +0530

    Updated Code at  17:26 16-02-2024

--- resources/views/frontend/countries.blade.php
+++ resources/views/frontend/countries.blade.php
@@ -902,10 +902,10 @@
             new Chart(ctx, {
                 type: 'bar',
                 data: {
-                labels: [countryNames[0], countryNames[1], countryNames[2], countryNames[3], countryNames[4], countryNames[5], countryNames[6], countryNames[7], countryNames[8]],
+                labels: [countryNames[0], countryNames[1], countryNames[2], countryNames[3], countryNames[4], countryNames[5], countryNames[6], countryNames[7], countryNames[8],countryNames[9]],
                 datasets: [{
                     label: '{{$country->cp_heading}}',
-                    data: [percentageData[0], percentageData[1], percentageData[2], percentageData[3], percentageData[4],percentageData[5], percentageData[6], percentageData[7],percentageData[8], percentageData[9]],percentageData[10]]
+                    data: [percentageData[0], percentageData[1], percentageData[2], percentageData[3], percentageData[4],percentageData[5], percentageData[6], percentageData[7],percentageData[8], percentageData[9]],
                     borderWidth: 1
                 }]
                 },
