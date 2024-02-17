5eeabc3eb48d3fab82d3c0a66e61e288fda83715
Power2913 <shakti29.uts@gmail.com>
2024-02-17 18:21:34 +0530

    SR_Commits-17-02-24

--- resources/views/frontend/continent.blade.php
+++ resources/views/frontend/continent.blade.php
@@ -413,7 +413,7 @@
                                 <img src="frontend/image/flags/democratic_republic_of_the_congo_rectangular_icon_with_shadow_64.png">
                                 <br>
                                 @foreach ($countrydata as $country)
-                                    @if ($country->country=='DR Congo' && $country->Datatype =='import')
+                                    @if ($country->country=='DR-Congo' && $country->Datatype =='import')
                                         <a href="{{ route('countryalldata', [ strtolower($country->country), $country->Datatype]) }}" class="text-hover text-white">
                                             <h4>{{ $country->country }}</h4>
                                         </a>
