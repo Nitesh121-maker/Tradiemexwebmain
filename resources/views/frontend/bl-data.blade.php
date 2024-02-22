20108309179b56dadf0ed3f60571e077c7d23b1f
Power2913 <shakti29.uts@gmail.com>
2024-02-22 15:57:36 +0530

    SR_Commits-22-02-24

--- resources/views/frontend/bl-data.blade.php
+++ resources/views/frontend/bl-data.blade.php
@@ -145,7 +145,7 @@
                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                         <div class="card-content">
                             <div class="text-content">
-                                <h2>TradeImeX: Democratizing B/L Data</h2>
+                                <h2 class="text-white">TradeImeX: Democratizing B/L Data</h2>
                                 <p>
                                     TradeImeX is revolutionizing the way businesses access and analyze B/L data. With their
                                     advanced platform, they provide users with seamless access to a comprehensive database of
@@ -158,13 +158,12 @@
                     <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
                         <div class="card-content">
                             <div class="text-content">
-                                <h2>Using B/L data to enhance business.</h2>
+                                <h2 class="text-white">Using B/L data to enhance business.</h2>
                                 <p>
                                     B/L data is crucial for businesses to thrive. TradeImeX offers a game-changing solution by
-                                    democratizing B/L data, providing businesses with unparalleled insights, and empowering them
-                                    to make informed decisions. By leveraging the B/L data of the advanced platform of TradeImeX,
-                                    businesses can gain a competitive edge, optimize their supply chain, and navigate the
-                                    complexities of international trade with confidence.
+                                    democratizing B/L data, providing businesses with unparalleled insights.By leveraging the 
+                                    B/L data of the advanced platform of TradeImeX,businesses can gain a competitive edge and 
+                                    navigate the complexities of international trade with confidence.
                                 </p>
                             </div>
                         </div>
