48f88ea43c4a63d28c1a2081ca6d3468552591c8
Power2913 <shakti29.uts@gmail.com>
2024-02-27 12:32:43 +0530

    SR_Commits-27-02-24

--- resources/views/frontend/link.blade.php
+++ resources/views/frontend/link.blade.php
@@ -35,3 +35,31 @@
     document.onclick=reEnable
     }
 </script>
+
+{{-- Disabling Inspect by buttons --}}
+<script>
+    document.onkeydown = (e) => {
+        if (e.key == 123) {
+            e.preventDefault();
+        }
+        if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+            e.preventDefault();
+        }
+        if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+            e.preventDefault();
+        }
+        if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+            e.preventDefault();
+        }
+        if (e.ctrlKey && e.key == 'U') {
+            e.preventDefault();
+        }
+    };
+</script>
+
+{{-- Disabling Right Click --}}
+<script>
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    })
+</script>
