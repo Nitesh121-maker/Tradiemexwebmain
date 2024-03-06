6af49bc3990fc23670701855bce858b16d9e6d91
Power2913 <shakti29.uts@gmail.com>
2024-03-06 18:09:07 +0530

    SR_Commits-06-03-24

--- resources/views/frontend/continent.blade.php
+++ resources/views/frontend/continent.blade.php
@@ -61,7 +61,6 @@
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
-                        <!-- <div id="chart_div" style="width: 500px; height: 500px;"></div> -->
                         @if ($continent->mf_content_images)
                             @php
                                 // Construct the full image URL using the base URL and the image filename.
@@ -1374,7 +1373,7 @@
             </div>
             <div class="container pdt-2">
                 <div  class="row">
-                    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8">
                         <figure class="highcharts-figure">
                             <div id="container-bar"></div>
                         </figure>
