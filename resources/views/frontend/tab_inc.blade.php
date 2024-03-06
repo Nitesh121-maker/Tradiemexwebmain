dbd72e6dcece02be5ad2b7a37fec2c684a909fe7
Power2913 <shakti29.uts@gmail.com>
2024-03-06 15:44:08 +0530

    SR_Commits-06-03-24

--- resources/views/frontend/tab_inc.blade.php
+++ resources/views/frontend/tab_inc.blade.php
@@ -1,4 +1,4 @@
-<div id="message1" class="container-fluid message-area bg-gradient-custom pdt-2 pdb-2">
+<div id="message1" class="container-fluid bg-gradient-custom pdt-2 pdb-2" style="text-align: center;">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-md-6 col-lg-6">
@@ -6,7 +6,7 @@
                     Are you ready to skyrocket your profits?Contact us. 
                 </div>
             </div>
-            <div class="col-sm-12 col-md-6 col-lg-6" style="display: flex;justify-content: end;">
+            <div class="col-sm-12 col-md-6 col-lg-6" style="display: flex;justify-content: center;">
                 <div class="buttons-holder">
                     <a href="/partners#sales" class="ybtn ybtn-accent-color">
                         Sales Partner
