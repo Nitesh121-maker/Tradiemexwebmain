96822bab21838772806fbb468a63363fe4cafadb
Power2913 <shakti29.uts@gmail.com>
2024-03-11 17:02:58 +0530

    SR_Commits-11-03-24

--- resources/views/frontend/global-trade-data.blade.php
+++ resources/views/frontend/global-trade-data.blade.php
@@ -596,7 +596,7 @@
                                         @endif
                                     @endforeach
                                 </div>
-                                <div class="col-sm-4 col-md-3 col-lg-2 Flag">
+                                {{-- <div class="col-sm-4 col-md-3 col-lg-2 Flag">
                                     <img src="frontend/image/flags/honduras_rectangular_icon_with_shadow_64.png">
                                     <br>
                                     @foreach ($countrydata as $country)
@@ -617,7 +617,7 @@
                                             </a>
                                         @endif
                                     @endforeach
-                                </div>
+                                </div> --}}
                             </div>
                             <div class="text-content">
                                 <h2 class="text-center text-white">South - America</h2>
